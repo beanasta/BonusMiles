8b34cc6944043803f1c6b6683c225381fd50a033
Anastasia <Beanasta@mail.ru>
2022-09-04 23:19:36 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 20000; // стоимость билета
+
+        int mileCost = 20; // стоимость 1 мили
+
+        int bonus = ticket / mileCost;
+
+        System.out.println("Количество начисленных миль за купленный билет:" + bonus);
+    }
+}
